8450652057f344ae5304671900942914964ac4fc
Maksim <maxorient1999@gmail.com>
2024-09-24 17:18:08 +0300

    init commit

---
+++ src/main/java/ru/discomfortdeliverer/lesson_five/LessonFiveApplication.java
@@ -0,0 +1,13 @@
+package ru.discomfortdeliverer.lesson_five;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonFiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/discomfortdeliverer/lesson_five/LessonFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.discomfortdeliverer.lesson_five;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonFiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
